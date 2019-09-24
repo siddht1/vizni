e4576961701c6e1d1541440c3fea55ffb54361a9
siddht4 <dhar.75.ap@gmail.com>
2019-09-24 11:58:54 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+// A simple web server
+// > npm install connect serve-static
+// > node server.js 8888
+var port = 80;
+if (process.argv.length > 2) port = parseInt(process.argv[2]);
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(port, function(){
+  console.log('Server running on '+port+'...');
+  console.log(connect)
+});
