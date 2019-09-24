82d2ad3c288be705f5a755c2b7624a84692b6166
siddht4 <dhar.75.ap@gmail.com>
2019-09-24 15:26:52 +0530

    Add files via upload

--- assets/js/app_base_module.js
+++ assets/js/app_base_module.js
@@ -1,23 +1,32 @@
-function  __data()
+
+async function  __data()
 {
   __file='/data/delhi.json'
-   
+   get_data(__file)
       // console.log(typeof(data))
-      load_json_xhr(__file)
+      // load_json_xhr(__file)
       // nsc(data)
     
     
   }
- function nsc(data)
+   async  function get_data(_url) 
+{
+  let response = await fetch(_url);
+  let data = await response.json()
+  nsc(data);
+}
+ async function nsc(data)
   {
     let params=({}, config);
-      params.transit = data;
+     params.transit = data
+    
       params.transit.legend = 0;
       params.transit.labels = 1;
       params.padding = [200, 100];
       params.animationDuration = 10000;
       app.views.main = new app.views.TransitShowView();
       app.views.main.render(params);
+      
     
   }
   
